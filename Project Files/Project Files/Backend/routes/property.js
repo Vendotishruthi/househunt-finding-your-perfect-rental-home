ee54b328108ecb00899e3e27634ef0509e13b565
Vendotishruthi <155349535+Vendotishruthi@users.noreply.github.com>
2026-02-26 18:42:51 +0530

    Delete Project Files directory

--- Project Files/Project Files/Backend/routes/property.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { addProperty, getProperties } from '../controllers/property.js';
-
-const router = express.Router();
-
-router.post('/', addProperty);
-router.get('/', getProperties);
-
-export default router;
